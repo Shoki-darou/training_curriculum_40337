601a5a212c929188ad03dcac6eba92968d5fb68c
Shoki-darou <shoki.rugby0115@gmail.com>
2024-02-23 12:49:49 +0900

    Issue6
    先ほどのコミットでIssue4とIssue5を一緒にコミットしてしまいました。

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,6 +37,7 @@ class CalendarsController < ApplicationController
         days = { 
           month: (@todays_date + x).month,
           date: (@todays_date + x).day,
+          day_of_week: wdays[(@todays_date + x).wday],
           plans: today_plans
         }
         @week_days.push(days)
